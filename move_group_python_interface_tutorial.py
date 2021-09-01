8984885e40d76cf5adfcd1d5ce2bdb51476992c9
AmnaMazen <47673149+AmnaMazen@users.noreply.github.com>
2021-09-01 12:01:56 -0400

    Add files via upload

---
+++ move_group_python_interface_tutorial.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial',
+                anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+
+scene = moveit_commander.PlanningSceneInterface()
+
+group_name = "widowx_arm"
+group = moveit_commander.MoveGroupCommander(group_name)
+
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+
+
+# We can get the name of the reference frame for this robot:
+planning_frame = group.get_planning_frame()
+print "============ Reference frame: %s" % planning_frame
+
+# We can also print the name of the end-effector link for this group:
+eef_link = group.get_end_effector_link()
+print "============ End effector: %s" % eef_link
+
+# We can get a list of all the groups in the robot:
+group_names = robot.get_group_names()
+print "============ Robot Groups:", robot.get_group_names()
+
+# Sometimes for debugging it is useful to print the entire state of the
+# robot:
+print "============ Printing robot state"
+print robot.get_current_state()
+print ""
+
+# Case 1:Planning to a Joint Goal
+# We can get the joint values from the group and adjust some of the values:
+joint_goal = group.get_current_joint_values()
+joint_goal[0] = 0
+joint_goal[1] = -pi/4
+joint_goal[2] = 0
+joint_goal[3] = -pi/2
+joint_goal[4] = 0
+
+
+
+# The go command can be called with joint values, poses, or without any
+# parameters if you have already set the pose or joint target for the group
+group.go(joint_goal, wait=True)
+
+# Calling ``stop()`` ensures that there is no residual movement
+group.stop()
+group.clear_pose_targets()
+
+# Make default position
+rospy.sleep(2);
+joint_goal = group.get_current_joint_values()
+joint_goal[0] = 0
+joint_goal[1] = 0
+joint_goal[2] = 0
+joint_goal[3] = 0
+joint_goal[4] = 0
+
+
+
+# The go command can be called with joint values, poses, or without any
+# parameters if you have already set the pose or joint target for the group
+group.go(joint_goal, wait=True)
+
+# Calling ``stop()`` ensures that there is no residual movement
+group.stop()
+group.clear_pose_targets()
+
+# Give command to open gripper
+
+rospy.sleep(2);
+group_name2 = "widowx_gripper"
+group2 = moveit_commander.MoveGroupCommander(group_name2)
+joint_goal2 = group2.get_current_joint_values()
+joint_goal2[0] = 0.03
+joint_goal2[1] = 0.03
+
+
+
+
+# The go command can be called with joint values, poses, or without any
+# parameters if you have already set the pose or joint target for the group
+group2.go(joint_goal2, wait=True)
+
+# Calling ``stop()`` ensures that there is no residual movement
+group2.stop()
+group2.clear_pose_targets()
